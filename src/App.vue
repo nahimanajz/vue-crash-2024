dbf068f25abeaa4a7dcaa40b278b7db70df97a3b
nahimanajz <jazzonahimana@gmail.com>
2024-08-14 21:57:52 +0200

    Add composition API
    Composition api using `<script setup>` is for making dynamic state

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,40 @@
+<script setup>// short form of using composition API
+import { ref } from "vue"
+//Composition API by it everything is housed in setup() methods
 
-<script>
-export default {
-  data(){
-    return {
-      name: "John doe",
-      status: true,
-      stringStatus:"pending",
-      tasks: ["task 1", "task 2", "task 3"],
-      link:"https://www.google.com"    }
-  },
-  methods:{
-    toggleStringStatus(){
-      if(this.stringStatus === "active"){
-        this.stringStatus = "pending"
-      } else if(this.stringStatus === "pending"){
-        this.stringStatus = "active"
-      } 
-    }
+const name = ref("John doe");
+const stringStatus = ref("active");
+const status = true;
+const tasks = ref(["task One", "Task two", "Task Three"]);
+
+const toggleStringStatus = () => {
+  if (stringStatus.value === "active") {
+    stringStatus.value = "pending"
+  } else if (stringStatus.value === "pending") {
+    stringStatus.value = "active"
   }
 }
+
+
+
 </script>
 
 <template>
-  <h1>{{  name}}</h1>
+  <h1>{{ name }}</h1>
   <p v-if="status"> User is active</p>
   <p v-else> User is inactive</p>
   <p v-if="stringStatus === 'active'"> User is active</p>
   <p v-if="stringStatus === 'pending'"> User is Pending</p>
-  
+
   <h3>Tasks</h3>
   <ul>
     <li v-for="task in tasks" :key="task"> {{ task }}</li>
   </ul>
-  <!-- <a v-bind:href="link">Click for google</a> -->
-  <a :href="link">Click for google</a>
-<br>
+
+  <br>
   <button @click="toggleStringStatus"> change status</button>
   <!-- <button v-on:click="toggleStringStatus"> change status</button> -->
 </template>
 
 
-<style>
-
-</style>
+<style></style>
