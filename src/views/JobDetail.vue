3f4aa4b7bef34e9310e23e3317bdfb072ca5a021
nahimanajz <jazzonahimana@gmail.com>
2024-08-16 01:01:32 +0200

    Delete a job

--- src/views/JobDetail.vue
+++ src/views/JobDetail.vue
@@ -1,28 +1,46 @@
 <script setup>
-import { reactive, onMounted } from 'vue'
+import { reactive, onMounted } from "vue";
 import axios from "axios";
-import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
-import { useRoute } from 'vue-router';
-import BackButton from '@/components/BackButton.vue';
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
+import { useRoute } from "vue-router";
+import BackButton from "@/components/BackButton.vue";
+import router  from '@/router';
+import { useToast } from 'vue-toastification'
 
 const state = reactive({
   job: {},
-  isLoading: true
-})
+  isLoading: true,
+});
 
-const route = useRoute()
-const jobId = route.params.id
+const route = useRoute();
+const jobId = route.params.id;
+const toast = useToast()
 onMounted(async () => {
   try {
-    const { data } = await axios.get(`/api/jobs/${jobId}`)
-    state.job = data
+    const { data } = await axios.get(`/api/jobs/${jobId}`);
+    state.job = data;
   } catch (error) {
-    console.error("failded to fetch job detail", error)
-  }
-  finally {
+    console.error("failded to fetch job detail", error);
+  } finally {
     state.isLoading = false;
   }
-})
+});
+const handleDelete =  async() => {
+  
+  
+  try {
+    const confirm = window.confirm("Are you sure you want to delete this job");
+    if (confirm) {
+      const response = await axios.delete(`/api/jobs/${jobId}`);
+      toast.success("Job deleted successfully");
+      router.push("/jobs")
+    }
+  } catch (error) {
+    console.error("failded to delete job", error);
+    toast.error("Job deleted successfully");
+  }
+    
+};
 </script>
 <template>
   <BackButton />
@@ -30,12 +48,16 @@ onMounted(async () => {
     <div class="container m-auto py-10 px-6">
       <div class="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6">
         <main>
-          <div class="bg-white p-6 rounded-lg shadow-md text-center md:text-left">
+          <div
+            class="bg-white p-6 rounded-lg shadow-md text-center md:text-left"
+          >
             <div class="text-gray-500 mb-4">{{ state.job.type }}</div>
             <h1 class="text-3xl font-bold mb-4">{{ state.job.title }}</h1>
-            <div class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start">
+            <div
+              class="text-gray-500 mb-4 flex align-middle justify-center md:justify-start"
+            >
               <i class="pi pi-map-marker text-lg text-orange-700 mr-2"></i>
-              
+
               <p class="text-orange-700">{{ state.job.location }}</p>
             </div>
           </div>
@@ -57,7 +79,6 @@ onMounted(async () => {
 
         <!-- Sidebar -->
         <aside>
-
           <div class="bg-white p-6 rounded-lg shadow-md">
             <h3 class="text-xl font-bold mb-6">Company Info</h3>
 
@@ -77,17 +98,23 @@ onMounted(async () => {
 
             <h3 class="text-xl">Contact Phone:</h3>
 
-            <p class="my-2 bg-green-100 p-2 font-bold">{{ state.job.company.contactPhone }}</p>
+            <p class="my-2 bg-green-100 p-2 font-bold">
+              {{ state.job.company.contactPhone }}
+            </p>
           </div>
           <!-- Manage -->
           <div class="bg-white p-6 rounded-lg shadow-md mt-6">
             <h3 class="text-xl font-bold mb-6">Manage Job</h3>
-            <RouterLink :to="`/jobs/edit/${state.job.id}`"
-              class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
-              Edit
-              Job</RouterLink>
+            <RouterLink
+              :to="`/jobs/edit/${state.job.id}`"
+              class="bg-green-500 hover:bg-green-600 text-white text-center font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+            >
+              Edit Job</RouterLink
+            >
             <button
-              class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+              @click="handleDelete"
+              class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+            >
               Delete Job
             </button>
           </div>
@@ -98,5 +125,4 @@ onMounted(async () => {
   <div v-else class="text-center text-gray-500 py-6">
     <PulseLoader />
   </div>
-
 </template>
